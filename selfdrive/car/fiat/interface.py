35d829cab400aee0408bfca2f38101620c1e787c
Patrick Bassut <patrickbassut@hotmail.com>
2025-01-18 11:45:30 -0300

    back to torque

--- selfdrive/car/fiat/interface.py
+++ selfdrive/car/fiat/interface.py
@@ -20,12 +20,14 @@ class CarInterface(CarInterfaceBase):
     # safety config
     ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.fiat)]
 
-    # CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
-    ret.lateralTuning.init('pid')
-    ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP  = [[9.  , 20. ], [9.   , 20.]]
-    # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV    = [[0.05, 0.15], [0.03 , 0.05]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV    = [[0.00, 0.00], [0.00 , 0.00]]
-    ret.lateralTuning.pid.kf = 0.00006
+    # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.30], [0.03, 0.05]] # chrysler
+    # ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV    = [[0.00, 0.00], [0.00 , 0.00]]
+    CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
+    # ret.lateralTuning.init('pid')
+    # ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP  = [[9.  , 20. ], [9.   , 20.]]
+    # ret.lateralTuning.pid.kpV , ret.lateralTuning.pid.kiV = [[0.8, 0.3], [0.00, 0.00]] # fastback
+    # ret.lateralTuning.pid.kf = 0.00004
+    # ret.lateralTuning.pid.kf = 0
 
     ret.centerToFront = ret.wheelbase * 0.44
     ret.enableBsm = False
