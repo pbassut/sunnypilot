69b9e521c9c3ad89ba56b78f0ce370964b815fc1
Patrick Bassut <patrickbassut@hotmail.com>
2025-01-18 14:25:40 -0300

    speed

--- selfdrive/car/fiat/interface.py
+++ selfdrive/car/fiat/interface.py
@@ -37,7 +37,8 @@ class CarInterface(CarInterfaceBase):
     ret.pcmCruise = not experimental_long
     ret.openpilotLongitudinalControl = experimental_long
 
-    ret.minSteerSpeed = 10 * CV.KPH_TO_MS
+    ret.minSteerSpeed = 5 * CV.KPH_TO_MS
+    ret.minEnableSpeed = 10 * CV.KPH_TO_MS
 
     # Tuning for experimental long
     ret.longitudinalTuning.kiV = [2.0, 1.5]
